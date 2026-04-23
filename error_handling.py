21b8d6b1eee25124c4e578304e1f72a49f45a527
MaGo1024 <olumideodusote@gmail.com>
2026-04-23 19:45:19 +0100

    Error Handling.

---
+++ error_handling.py
@@ -0,0 +1,27 @@
+# FileNotFound
+
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("The file has been closed.")
+
+
+height = float(input("Please enter your height in meters: "))
+weight = float(input("Please enter your weight in kilograms: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters")
+
+bmi = weight/height ** 2
+print(bmi)
